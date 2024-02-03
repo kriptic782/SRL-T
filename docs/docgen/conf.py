3b0c00c9c648a8c123f2c859389fa9511791070e
Torwent <Torwent@users.noreply.github.com>
2024-02-03 21:04:10 +0100

    fix(docs): documentation migration to use markdown with sphinx myst parser
    - This was mostly automated and some documentation likely broke. e.g. Images are likely broken

--- docs/docgen/conf.py
+++ docs/docgen/conf.py
@@ -20,6 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = u'SRL-T'
+description = u'SRL-T is a fork of SRL-Development. SRL is a library for Simba to interact with RuneScape.'
 copyright = u'2020, SRL Group'
 author = u'SRL Group'
 
@@ -41,7 +42,10 @@ release = u''
 extensions = [
     'sphinx.ext.githubpages',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
+    'sphinx.ext.autosectionlabel',
+    'sphinx_copybutton',
+    'myst_parser'
 ]
 
 autodoc_default_flags = ['members']
@@ -54,7 +58,7 @@ templates_path = ['.']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -80,7 +84,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -109,7 +113,7 @@ html_theme_options = {
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'SRL-Tdoc'
+htmlhelp_basename = project+'doc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -135,20 +139,14 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'SRL-T.tex', u'SRL-T Documentation',
-     u'SRL Group', 'manual'),
-]
+latex_documents = [(master_doc, project+'.tex', project+' Documentation', author, 'manual')]
 
 
 # -- Options for manual page output ------------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'SRL-T', u'SRL-T Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, project, project+' Documentation',[author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------------
@@ -156,12 +154,12 @@ man_pages = [
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'SRL-T', u'SRL-T Documentation',
-     author, 'SRL', 'One line description of project.',
-     'Miscellaneous'),
-]
+texinfo_documents = [(master_doc, project, project+' Documentation', author, project, description,'Miscellaneous')]
 
 
 # -- Extension configuration -------------------------------------------------
 
+myst_enable_extensions = [
+ 'linkify'
+]
+sphinxemoji_style = 'twemoji'
